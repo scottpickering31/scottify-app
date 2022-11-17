3781ac8b8ae8d528c6a39144c69e37ec6efc36f5
Scott Pickering <scottpickering31@gmail.com>
2022-11-17 12:17:17 +0000

    Updates

---
+++ precache-manifest.d84948b18a97601a4e2622fc0af5a3dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9a6d0d55d58b21c963",
+    "url": "/scottify-app/static/css/main.573f6138.chunk.css"
+  },
+  {
+    "revision": "2f9a6d0d55d58b21c963",
+    "url": "/scottify-app/static/js/main.2f9a6d0d.chunk.js"
+  },
+  {
+    "revision": "9a706a69ef0a8c3185a3",
+    "url": "/scottify-app/static/js/1.9a706a69.chunk.js"
+  },
+  {
+    "revision": "43014aa8e0b8249a12b6",
+    "url": "/scottify-app/static/js/2.43014aa8.chunk.js"
+  },
+  {
+    "revision": "ffeb75de2808f9b52cf0",
+    "url": "/scottify-app/static/js/runtime~main.ffeb75de.js"
+  },
+  {
+    "revision": "3da36b41eedf5d2e3ad4f5edb235a3f1",
+    "url": "/scottify-app/index.html"
+  }
+];
